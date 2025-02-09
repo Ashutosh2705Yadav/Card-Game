8381671bc39198ef045b88c4190b79d53ba2b424
Ashutosh <ashutosh27052003@gmail.com>
2025-02-09 22:58:03 +0530

    Added all the rules for wining

--- js/main.js
+++ js/main.js
@@ -23,18 +23,22 @@ function getCards(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        console.log(data)
         console.log(data.cards[0].image)
         console.log(data.cards[1].image)
         document.querySelector("#Player1").src = data.cards[0].image
         document.querySelector("#Player2").src = data.cards[1].image
-        let player1Val = data.cards[0].value
-        let player2Val = data.cards[1].value
+        let player1Val = valOfCard(data.cards[0].value)
+        let player2Val = valOfCard(data.cards[1].value)
         if(player1Val>player2Val){
           document.querySelector("h3").innerText = "🏆 The Winner Is Player 1"
 
         }
-        else{
+        else if(player1Val==player2Val){
           document.querySelector("h3").innerText = "🏆 The Winner Is Player 2"
+        }
+        else{
+          document.querySelector("h3").innerText = "Lets go to War"
         }
       
         
@@ -43,4 +47,25 @@ function getCards(){
           console.log(`error ${err}`)
       });
 }
+
+function valOfCard(val){
+  if(val == "KING"){
+    return 13
+  }
+  else if(val=="JACK"){
+    return 11
+  }
+  else if(val=="QUEEN"){
+    return 12
+  }
+  else if(val=="ACE"){
+    return 14
+  }
+  else{
+    return val
+  }
+    
+
+}
+
 
